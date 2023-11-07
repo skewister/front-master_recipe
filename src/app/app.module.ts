eb4d6fd8031dcbfc35d4ffe05e8c0e406edae245
Leo Lacrabere <gofriends9@gmail.com>
2023-11-07 10:50:43 +0100

    Création des pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RecipesListComponent } from './pages/recipes-list/recipes-list.component';
+import { RecipeDetailComponent } from './pages/recipe-detail/recipe-detail.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HomeComponent,
+    RecipesListComponent,
+    RecipeDetailComponent,
+    LoginComponent,
+    RegisterComponent,
+    UserProfileComponent
   ],
   imports: [
     BrowserModule,
