4d26fd1bc3b9ac451962520348d993ddfe7b7591
Leo Lacrabere <gofriends9@gmail.com>
2023-12-16 11:41:35 +0100

    url universel pour tous

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8000/api',
+  Url:  'http://localhost:8000'
+};
+
+export const API_URL= 'http://localhost:8000/api'
