65cf88fcdbbf4a03b63e24a73b7f9ed17d58764b
Leo Lacrabere <gofriends9@gmail.com>
2024-01-14 23:14:50 +0100

    Ajout de daysi ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
+
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
